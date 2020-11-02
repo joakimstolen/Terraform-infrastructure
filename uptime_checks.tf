49bebc01f2b969e1acea87565801ebe9b6df8f88
joakimstolen <joakimstoelen@hotmail.com>
2020-11-02 12:08:47 +0100

    changed check_rate

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -8,7 +8,7 @@ resource "statuscake_test" "google" {
   test_type = "HTTP"
   website_name = "My test 2"
   website_url = "https://letshopethisdoesnotexistatall123.com"
-  check_rate = 300
+  check_rate = 10
   contact_group = ["193689"]
 }
 
