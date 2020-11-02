51ea98179141f723a3e92a877046b2f313332689
joakimstolen <joakimstoelen@hotmail.com>
2020-11-02 11:50:37 +0100

    added new url

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -7,7 +7,7 @@ provider "statuscake" {
 resource "statuscake_test" "google" {
   test_type = "HTTP"
   website_name = "My test"
-  website_url = "https://letshopethisdoesnotexistatall.com"
+  website_url = "https://letshopethisdoesnotexistatall123.com"
   check_rate = 300
   contact_group = ["193689"]
 }
