b6936e4bb433439a85c3038dd85755c7066b7cc4
joakimstolen <joakimstoelen@hotmail.com>
2020-11-04 11:55:20 +0100

    Changed url to avoid notifications..

--- uptime_checks.tf
+++ uptime_checks.tf
@@ -7,7 +7,7 @@ provider "statuscake" {
 resource "statuscake_test" "google" {
   test_type = "HTTP"
   website_name = "My test 3"
-  website_url = google_cloud_run_service.hello.status[0].url
+  website_url = "www.thisdoesnotexisthopefully123.com"
   check_rate = 10
   contact_group = ["193689"]
 }
